90a5926f1b9ca662472093dddcef3b0d879556f6
Cyan0guy <91061384+Cyan0guy@users.noreply.github.com>
2021-09-21 12:49:01 +0530

    Add files via upload

---
+++ 2/src/test/java/com/tdd/AddStringTest.java
@@ -0,0 +1,18 @@
+package com.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddStringTest {
+
+	@Test
+	void test() {
+		AddString a=new AddString();
+		assertEquals(0, a.Add(""));
+		assertEquals(1, a.Add("1"));
+		assertEquals(3, a.Add("1,2"));
+		assertEquals(27, a.Add("1,3,5,7,11"));
+	}
+
+}
